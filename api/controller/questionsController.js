e1b98623fa459fc12cd35d067cea49549996299b
deol.engg <er.jaskarandeol@gmail.com>
2018-04-13 23:31:35 -0400

    { Question Route : Update }

--- api/controller/questionsController.js
+++ api/controller/questionsController.js
@@ -20,39 +20,42 @@ exports.addQuestion = function (req, res) {
         if (err)
             res.send(err)
         else {
-            res.status(202).send({ Success :  'Question Accepted and Added'});
+            res.status(202).send({ Success: 'Question Accepted and Added' });
         }
     });
 };
 
 
-exports.newProvideQuestionairePerService = function (req, res) {
+exports.provideQuestionairePerService = function (req, res) {
     //return which service is required  
-    let serviceName = req.query.service; 
-    Service.find({ name : serviceName }, (err, service) => { 
+    let serviceName = req.params.service;
+    console.log(req.params.service);
+
+    Service.find({ name: serviceName }, (err, service) => {
         if (err) { console.log(err); throw err; }
-        if (service.length == 1){
-            service = service [0];
-            if (service.available == false) return res.status(401).send({Error : 'Service Not Available' }); 
+        if (service.length == 1) {
+            service = service[0];
+            if (service.available == false)
+                return res.status(401).send({ Error: 'Service Not Available' });
 
             console.log("This Service is Available");
-            console.log(typeof service.questions); 
-           
+            console.log(typeof service.questions);
+
             let questionsPromises = [];
             service.questions.forEach(question => {
-                questionsPromises.push(Questions.findOne({id: question}));
+                questionsPromises.push(Questions.findOne({ id: question }));
             });
 
             Promise.all(questionsPromises).then(questions => {
                 res.send(questions.map(ques => {
-                    return (({id,type,format ,multiple,options,filter}) => 
-                    ({id,type,format ,multiple,options,filter}))(ques);
+                    return (({ id, type, format, multiple, options, filter }) =>
+                        ({ id, type, format, multiple, options, filter }))(ques);
                 }));
             })
         }
         else {
-            console.log("Service Called : "+serviceName+" is Invalid");
-            return res.status(404).send({Error : 'Service Not Found' }); 
+            console.log("Service Called : " + serviceName + " is Invalid");
+            return res.status(404).send({ Error: 'Service Not Found' });
         }
     });
 };
--- api/route/questionsRoute.js
+++ api/route/questionsRoute.js
@@ -2,18 +2,19 @@
 
 module.exports = function (app) {
     var questions = require('../controller/questionsController');
-    
-    app.use(function(req, res, next) {
+
+    app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
-      });
-      
-    app.route('/services/questionaire')
-    .get(questions.listQuestions) 
-    .post(questions.addQuestion); 
+    });
 
-    app.route('/:service')
-    .get(questions.newProvideQuestionairePerService);
-    
+
+    app.route('/questionaire/:service')
+        .get(questions.provideQuestionairePerService);
+
+    app.route('/services/questionaire')
+        .get(questions.listQuestions)
+        .post(questions.addQuestion);
+
 }
